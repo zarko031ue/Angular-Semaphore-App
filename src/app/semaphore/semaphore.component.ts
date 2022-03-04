f8f1d03ccfba3ad273f40c01ff47c2219a6a43ca
Zarkom <zarkosavic111@gmail.com>
2022-03-04 16:07:34 +0100

    Set yellow color

--- src/app/semaphore/semaphore.component.ts
+++ src/app/semaphore/semaphore.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-semaphore',
   templateUrl: './semaphore.component.html',
@@ -10,22 +9,30 @@ export class SemaphoreComponent implements OnInit {
   randomNumber: number;
   circles = [];
   circlesCount = 8;  
-
+  
   constructor() {}
 
   ngOnInit() {
+    this.yellow();
     const interval = setInterval(() => this.start(), 3000)
-    setTimeout(() => clearInterval(interval), 30000)
+    setTimeout(() => clearInterval(interval), 10000)
   }
 
   start() {
     const randNumber = this.getRandomNumber();
     this.convertFromDecToBin(randNumber);
   }
-
+  
   getRandomNumber() {
-     this.randomNumber = Math.floor(Math.random() * 256) + 1; 
-     return this.randomNumber;
+     this.randomNumber = Math.floor(Math.random() * 256) + 1;
+      return this.randomNumber;
+  }
+
+// Znao sam da ovo nece da radi ali hajde da komitujem bilo sta
+  yellow(){
+    this.circles.forEach(circle => {
+    return circle === 'yellow';
+    })
   }
 
   convertFromDecToBin(number){
@@ -44,35 +51,13 @@ export class SemaphoreComponent implements OnInit {
     while (binaryPosition < this.circlesCount){
         binaryNumber[binaryPosition++] = 0;
     }
-
+    
     this.circles = binaryNumber
-    console.log(this.circles)
-
+   
     return this.circles 
 }
 
 
-// Ponovo sam koristio funkciju koju smo prepravljali jer ova ne vraca 8-bitni broj
-// Verovatno sam totalno promasio pristup, jer ne mogu da inicijalizujem pocetnu zutu boju semafora
-
-//  convertFromDecToBin(number){
-//   var binaryNumber = [];
-  
-
-//   for (let i = number; i > 0; i--) {
-//     binaryNumber.push(number % 2);
-//     number = Math.floor(number / 2)
-//   }
-
-//   if (binaryNumber.length < this.circles.length) {
-//     for (let i = this.circles.length - binaryNumber.length; i < this.circles.length; i++) {
-//       binaryNumber.push(0);
-//   }
-// }
-//   this.circles = binaryNumber;
-// console.log(this.circles);
-//   return this.circles
-// }
  
 }
 
